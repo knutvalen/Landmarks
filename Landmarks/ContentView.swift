9c60cdd0e4c1ca9774819c3f4651048c66228f5c
Knut Valen <knut.valen@icloud.com>
2024-06-29 20:05:33 +0200

    Initial commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Knut Valen on 27/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+            
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Knut Valen on 27/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
