7e2a5a3ce7983a620868d75ed5fcb3ccd573e43a
Knut Valen <knut.valen@icloud.com>
2024-07-02 12:08:48 +0200

    Mark favorite landmarks

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -12,9 +12,18 @@ struct LandmarkRow: View {
 
     var body: some View {
         HStack {
-            landmark.image.resizable().frame(width: 50, height: 50)
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+
             Text(landmark.name)
+
             Spacer()
+
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundStyle(.yellow)
+            }
         }
     }
 }
