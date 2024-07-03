496e21acce06e06dc1ea7dcc8d4a9e915a605ea5
Knut Valen <knut.valen@icloud.com>
2024-07-03 14:50:29 +0200

    Create the view controller’s data source

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -9,18 +9,65 @@ import SwiftUI
 import UIKit
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        // MARK: Lifecycle
+
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+
+        // MARK: Internal
+
+        var parent: PageViewController
+        var controllers = [UIViewController]()
+
+        func pageViewController(
+            _: UIPageViewController,
+            viewControllerBefore viewController: UIViewController
+        ) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return controllers.last
+            }
+            return controllers[index - 1]
+        }
+
+        func pageViewController(
+            _: UIPageViewController,
+            viewControllerAfter viewController: UIViewController
+        ) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            return controllers[index + 1]
+        }
+    }
+
     var pages: [Page]
 
-    func updateUIViewController(_ uiViewController: UIPageViewController, context _: Context) {
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func updateUIViewController(_ uiViewController: UIPageViewController, context: Context) {
         uiViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true
+            [context.coordinator.controllers[0]], direction: .forward, animated: true
         )
     }
 
-    func makeUIViewController(context _: Context) -> UIPageViewController {
-        return UIPageViewController(
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal
         )
+        pageViewController.dataSource = context.coordinator
+
+        return pageViewController
     }
 }
