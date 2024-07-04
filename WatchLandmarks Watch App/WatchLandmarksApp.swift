1ea812fdb5b26529470d81cc00c53de65bde1c49
Knut Valen <knut.valen@icloud.com>
2024-07-04 12:49:10 +0200

    Add a watchOS target

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Knut Valen on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchLandmarks Watch AppTests/WatchLandmarks_Watch_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  WatchLandmarks_Watch_AppTests.swift
+//  WatchLandmarks Watch AppTests
+//
+//  Created by Knut Valen on 04/07/2024.
+//
+
+@testable import WatchLandmarks_Watch_App
+import XCTest
+
+final class WatchLandmarks_Watch_AppTests: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Tests marked async will run the test method on an arbitrary thread managed by the Swift runtime.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
