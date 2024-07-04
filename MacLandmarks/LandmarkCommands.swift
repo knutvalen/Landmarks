8c04bc100dc782167b269f99e562e44485cea5a8
Knut Valen <knut.valen@icloud.com>
2024-07-04 14:53:03 +0200

    Add a built-in menu command

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  MacLandmarks
+//
+//  Created by Knut Valen on 04/07/2024.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
