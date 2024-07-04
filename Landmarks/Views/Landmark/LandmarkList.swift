afdf724ba89597f1ad9d7cf0810feeb61cb1c2c6
Knut Valen <knut.valen@icloud.com>
2024-07-04 14:43:54 +0200

    Update the list view

--- Landmarks/Views/Landmark/LandmarkList.swift
+++ Landmarks/Views/Landmark/LandmarkList.swift
@@ -10,20 +10,34 @@ import SwiftUI
 struct LandmarkList: View {
     // MARK: Internal
 
+    enum FilterCategory: String, CaseIterable, Identifiable {
+        case all = "All"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+
+        // MARK: Internal
+
+        var id: FilterCategory { self }
+    }
+
     @Environment(ModelData.self) var modelData
 
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
-            !showFavoritesOnly || landmark.isFavorite
+            (!showFavoritesOnly || landmark.isFavorite)
+                && (filter == .all || filter.rawValue == landmark.category.rawValue)
         }
+    }
+
+    var title: String {
+        let title = filter == .all ? "Landmarks" : filter.rawValue
+        return showFavoritesOnly ? "Favorite \(title)" : title
     }
 
     var body: some View {
         NavigationSplitView {
             List {
-                Toggle(isOn: $showFavoritesOnly, label: {
-                    Text("Favorites only")
-                })
                 ForEach(filteredLandmarks) { landmark in
                     NavigationLink {
                         LandmarkDetail(landmark: landmark)
@@ -33,8 +47,26 @@ struct LandmarkList: View {
                 }
             }
             .animation(.default, value: filteredLandmarks)
-            .navigationTitle("Landmarks")
+            .navigationTitle(title)
             .frame(minWidth: 300)
+            .toolbar {
+                ToolbarItem {
+                    Menu {
+                        Toggle(isOn: $showFavoritesOnly, label: {
+                            Text("Favorites only")
+                        })
+
+                        Picker("Category", selection: $filter) {
+                            ForEach(FilterCategory.allCases) { category in
+                                Text(category.rawValue).tag(category)
+                            }
+                        }
+                        .pickerStyle(.inline)
+                    } label: {
+                        Label("Filter", systemImage: "slider.horizontal.3")
+                    }
+                }
+            }
         } detail: {
             Text("Select a Landmark")
         }
@@ -43,6 +75,7 @@ struct LandmarkList: View {
     // MARK: Private
 
     @State private var showFavoritesOnly = false
+    @State private var filter = FilterCategory.all
 }
 
 #Preview {
